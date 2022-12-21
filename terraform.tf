a01b7494709e4ca4ea07eacf0fbaad2216356d4d
Tom Wieczorek <twieczorek@mirantis.com>
2022-12-21 17:04:07 +0100

    Use Terraform 1.3

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,6 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = ">= 1.3.0"
 
-  experiments = [
-    module_variable_optional_attrs,
-  ]
   required_providers {
     tls      = { source = "hashicorp/tls", version = "~> 4.0", }
     local    = { source = "hashicorp/local", version = "~> 2.0", }
