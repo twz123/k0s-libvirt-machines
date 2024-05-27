a597ad29fe2d1e4fa2a3a9e61ab39bfce0b0ede4
Tom Wieczorek <twieczorek@mirantis.com>
2024-05-27 10:56:35 +0200

    Update Terraform modules
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- modules/machine/terraform.tf
+++ modules/machine/terraform.tf
@@ -3,6 +3,6 @@ terraform {
 
   required_providers {
     random  = { source = "hashicorp/random", version = "~> 3.0", }
-    libvirt = { source = "dmacvicar/libvirt", version = "~> 0.6.0", }
+    libvirt = { source = "dmacvicar/libvirt", version = "~> 0.7.0", }
   }
 }
--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,6 @@ terraform {
     http     = { source = "hashicorp/http", version = "~> 2.0", }
     null     = { source = "hashicorp/null", version = "~> 3.0", }
     external = { source = "hashicorp/external", version = "~> 2.0", }
-    libvirt  = { source = "dmacvicar/libvirt", version = "~> 0.6.0", }
+    libvirt  = { source = "dmacvicar/libvirt", version = "~> 0.7.0", }
   }
 }
